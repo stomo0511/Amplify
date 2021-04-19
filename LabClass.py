4a2d80aaa22326596cba3e336006755b8ab73b98
Tomo Suzuki <stomo0511+atlassian@gmail.com>
2021-04-19 18:37:07 +0900

    コストのみ

--- LabClass.py
+++ LabClass.py
@@ -17,6 +17,8 @@ from amplify.client import FixstarsClient
 client = FixstarsClient()
 client.token = "jtSrmOum5m4eTuMEKDbrBekiOqa6nkCg"
 client.parameters.timeout = 5000  # タイムアウト5秒
+# client.parameters.outputs.duplicate = True  # 同じエネルギー値の解を列挙するオプション
+# client.parameters.outputs.num_outputs = 0   # 見つかったすべての解を出力
 
 # ソルバの生成
 solver = Solver(client)
@@ -36,12 +38,29 @@ students = [4, 5, 4, 3, 3, 2, 4, 4, 4, 4, 5, 3, 4, 3, 3]
 assert nlab == len(students), '研究室数と学生数配列の長さが違う'
 
 # グループ名
-grps = ["CS1", "CS2", "CS3", "CS"]
+grps = ["CS1", "CS2", "CS3", "CS4"]
 ngrp = len(grps)   # グループ数
 
 ##################################################################################
-# 変数の生成: ncity x nicity
+# QUBO変数の生成: nlab x ngrp
 q = gen_symbols(BinaryPoly, nlab, ngrp)
+
+# 研究室の教員数
+T = [
+    sum_poly( [q[i][j] * teachers[i] for i in range(nlab)] ) for j in range(ngrp)
+]
+
+# 研究室の学生数
+S = [
+    sum_poly( [q[i][j] * students[i] for i in range(nlab)] ) for j in range(ngrp)
+]
+
+##################################################################################
+# コスト関数：グループの学生数が等しいか？
+cost = sum_poly(
+    ngrp,
+    lambda j: (S[j] - S[(j+1) % ngrp])**2
+)
 
 ##################################################################################
 # 行（研究室）に対する制約： one-hot制約（1つの研究室が属するグループは1つだけ）
@@ -53,7 +72,8 @@ row_constraints = [
 constraints = sum(row_constraints)
 
 # モデル
-model = constraints
+# model = cost + constraints
+model = cost
 
 # ソルバ起動
 result = solver.solve(model)
@@ -61,9 +81,21 @@ result = solver.solve(model)
 if len(result) == 0:
     raise RuntimeError("Any one of constraints is not satisfied.")
 
-for sol in result:
-    energy = sol.energy
-    values = sol.values
-    q_values = decode_solution(q, values)
+energy = result[0].energy
+values = result[0].values
+q_values = decode_solution(q, values)
     
-    print(f"energy = {energy}, {q} = {q_values}")
+print(f"エネルギー: {energy}")
+
+for j in range(ngrp):
+    print(f"グループ {grps[j]} の教員数: {sum_poly([q_values[i][j] * teachers[i] for i in range(nlab)])}, 学生数: {sum_poly([q_values[i][j] * students[i] for i in range(nlab)])}")
+print()
+for j in range(ngrp):
+    print(f"グループ {grps[j]} の教員: ", end="")
+    for i in range(nlab):
+        if (q_values[i][j] == 1):
+            print(labs[i], ", ", end="")
+    print()
+print()
+for i in range(nlab):
+    print(f"{labs[i]} : {sum_poly([q_values[i][j] for j in range(ngrp)])}")
