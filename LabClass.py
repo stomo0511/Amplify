82e76ad0c1d5003c97b0cc6c9b17708981028c42
Tomo Suzuki <stomo0511+atlassian@gmail.com>
2021-04-20 14:43:10 +0900

    2020グループ分け情報追加

--- LabClass.py
+++ LabClass.py
@@ -29,11 +29,11 @@ nlab = len(labs)  # 研究室数
 
 # 研究室教員数
 nteachers = [1, 1, 1, 2, 1, 1, 1, 1, 2, 1, 2, 1, 2, 1, 1]
-assert nlab == len(nteachers), '研究室数と教員数配列の長さが違う'
+assert nlab == len(nteachers), "研究室数と教員数配列の長さが違う"
 
 # 研究室学生数
 nstudents = [4, 5, 4, 3, 3, 2, 4, 4, 4, 4, 5, 3, 4, 3, 3]
-assert nlab == len(nstudents), '研究室数と学生数配列の長さが違う'
+assert nlab == len(nstudents), "研究室数と学生数配列の長さが違う"
 
 # グループ名
 grps = ["CS1", "CS2", "CS3", "CS4"]
@@ -44,13 +44,13 @@ ngrp = len(grps)   # グループ数
 q = gen_symbols(BinaryPoly, nlab, ngrp)
 
 # 幹事研究室の指定
-# CS1: Ando(0), CS2: Kinoshita(13), CS3: Watanabe(9), CS4: Toyoura(1)
-# （上のカッコ内は研究室インデックス）
-q[0][0]  = BinaryPoly(1)
-q[13][1] = BinaryPoly(1)
-q[9][2]  = BinaryPoly(1)
-q[1][3]  = BinaryPoly(1)
+# CS1: Ando, CS2: Kinoshita, CS3: Watanabe, CS4: Toyoura
+q[labs.index("Ando")][grps.index("CS1")] = BinaryPoly(1)
+q[labs.index("Kinoshita")][grps.index("CS2")] = BinaryPoly(1)
+q[labs.index("Watanabe")][grps.index("CS3")] = BinaryPoly(1)
+q[labs.index("Toyoura")][grps.index("CS4")] = BinaryPoly(1)
 
+##################################################################################
 # 研究室の教員数
 T = [
     sum_poly( [q[i][j] * nteachers[i] for i in range(nlab)] ) for j in range(ngrp)
@@ -61,8 +61,7 @@ S = [
     sum_poly( [q[i][j] * nstudents[i] for i in range(nlab)] ) for j in range(ngrp)
 ]
 
-##################################################################################
-# コスト関数：グループの学生数、教員数が等しいか？
+# コスト関数：グループの学生数、教員数が等しいとき 0
 cost = sum_poly(
     ngrp,
     lambda j: (S[j] - S[(j+1) % ngrp])**2
@@ -72,11 +71,45 @@ cost = sum_poly(
 )
 
 ##################################################################################
+# 前年度と同じグループにならないようにする（ハード制約）
+q2020 = gen_symbols(BinaryPoly, nlab, ngrp)
+
+# ０で初期化
+for i in range(nlab):
+    for j in range(ngrp):
+        q2020[i][j] = BinaryPoly(0)
+
+# 2020年度のグループ分け
+# CS1: Takahashi, Fukumoto, Toyoura
+q2020[labs.index("Takahashi")][grps.index("CS1")] = BinaryPoly(1)
+q2020[labs.index("Fukumoto")][grps.index("CS1")] = BinaryPoly(1)
+q2020[labs.index("Toyoura")][grps.index("CS1")] = BinaryPoly(1)
+
+# CS2: Go, Ozawa, Mao, Ohbushi
+q2020[labs.index("Go")][grps.index("CS2")] = BinaryPoly(1)
+q2020[labs.index("Ozawa")][grps.index("CS2")] = BinaryPoly(1)
+q2020[labs.index("Mao")][grps.index("CS2")] = BinaryPoly(1)
+q2020[labs.index("Ohbuchi")][grps.index("CS2")] = BinaryPoly(1)
+
+# CS3: Omata, Ando, Hattori, Watanabe
+q2020[labs.index("Omata")][grps.index("CS3")] = BinaryPoly(1)
+q2020[labs.index("Ando")][grps.index("CS3")] = BinaryPoly(1)
+q2020[labs.index("Hattori")][grps.index("CS3")] = BinaryPoly(1)
+q2020[labs.index("Watanabe")][grps.index("CS3")] = BinaryPoly(1)
+
+# CS4: Suzuki, Iwanuma, Kinoshita, Nabeshima
+q2020[labs.index("Suzuki")][grps.index("CS3")] = BinaryPoly(1)
+q2020[labs.index("Iwanuma")][grps.index("CS3")] = BinaryPoly(1)
+q2020[labs.index("Kinoshita")][grps.index("CS3")] = BinaryPoly(1)
+q2020[labs.index("Nabeshima")][grps.index("CS3")] = BinaryPoly(1)
+
+##################################################################################
 # 行（研究室）に対する制約： one-hot制約（1つの研究室が属するグループは1つだけ）
 row_constraints = [
     equal_to(sum_poly([q[i][j] for j in range(ngrp)]), 1) for i in range(nlab)
 ]
 
+##################################################################################
 # 制約
 constraints = sum(row_constraints)
 
@@ -86,32 +119,33 @@ model = cost + 10*constraints
 # ソルバの生成
 solver = Solver(client)
 
-# ソルバ起動
-result = solver.solve(model)
-
-# 解が見つからないときのエラー出力
-if len(result) == 0:
-    raise RuntimeError("Any one of constraints is not satisfied.")
-
-energy = result[0].energy
-values = result[0].values
-q_values = decode_solution(q, values)
-
 ##################################################################################
-# 結果の表示   
-print(f"エネルギー: {energy}")
+# ソルバ起動
+# result = solver.solve(model)
 
-for j in range(ngrp):
-    print(f"グループ {grps[j]} の教員数: {sum_poly([q_values[i][j] * nteachers[i] for i in range(nlab)])}, 学生数: {sum_poly([q_values[i][j] * nstudents[i] for i in range(nlab)])}")
-print()
-print("各グループの研究室の表示")
-for j in range(ngrp):
-    print(f"グループ {grps[j]} の教員: ", end="")
-    for i in range(nlab):
-        if (q_values[i][j] == 1):
-            print(labs[i], ", ", end="")
-    print()
-print()
-print("制約の確認（研究室が一度ずつ現れているか）")
-for i in range(nlab):
-    print(f"{labs[i]} : {sum_poly([q_values[i][j] for j in range(ngrp)])}")
+# # 解が見つからないときのエラー出力
+# if len(result) == 0:
+#     raise RuntimeError("Any one of constraints is not satisfied.")
+
+# energy = result[0].energy
+# values = result[0].values
+# q_values = decode_solution(q, values)
+
+# ##################################################################################
+# # 結果の表示   
+# print(f"エネルギー: {energy}")
+
+# for j in range(ngrp):
+#     print(f"グループ {grps[j]} の教員数: {sum_poly([q_values[i][j] * nteachers[i] for i in range(nlab)])}, 学生数: {sum_poly([q_values[i][j] * nstudents[i] for i in range(nlab)])}")
+# print()
+# print("各グループの研究室の表示")
+# for j in range(ngrp):
+#     print(f"グループ {grps[j]} の教員: ", end="")
+#     for i in range(nlab):
+#         if (q_values[i][j] == 1):
+#             print(labs[i], ", ", end="")
+#     print()
+# print()
+# print("制約の確認（研究室が一度ずつ現れているか）")
+# for i in range(nlab):
+#     print(f"{labs[i]} : {sum_poly([q_values[i][j] for j in range(ngrp)])}")
