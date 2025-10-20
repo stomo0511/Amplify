461829d9ebc7ac2cc6b520a1d8108e57c27f38b6
Tomohiro Suzuki <stomo@yamanashi.ac.jp>
2025-10-20 14:28:46 +0900

    GraphColor

--- Tutorial_Color.py
+++ Tutorial_Color.py
@@ -1,64 +1,59 @@
-from amplify import (
-    BinaryPoly,
-    BinaryQuadraticModel,
-    sum_poly,
-    gen_symbols,
-    Solver,
-    decode_solution,
-)
-from amplify.constraint import (
-    equal_to, 
-    penalty,
-)
-from amplify.client import FixstarsClient
-import numpy as np
+from amplify import VariableGenerator
 import japanmap as jm
-import matplotlib.pyplot as plt
-
-client = FixstarsClient()
-client.token = "i5G6Ei3DKlGv2n6hsWBSBzWrmffLN4vn"  #20210011まで有効
-client.parameters.timeout = 5000  # タイムアウト5秒
-
-solver = Solver(client)
-
-# 四色の定義
+    
 colors = ["red", "green", "blue", "yellow"]
 num_colors = len(colors)
-num_region = len(jm.pref_names) - 1  # 都道府県数を取得
+num_region = len(jm.pref_names)
+    
+gen = VariableGenerator()
+q = gen.array("Binary", shape=(num_region, num_colors))
 
-# 都道府県数 x 色数 の変数を作成
-q = gen_symbols(BinaryPoly, num_region, num_colors)
-
+# 制約条件
+from amplify import sum as amplify_sum, one_hot, equal_to
+    
 # 各領域に対する制約
-# 一つの領域に一色のみ（one-hot）
-# sum_{c=0}^{C-1} q_{i,c} = 1 for all i
-reg_constraints = [
-    equal_to(sum_poly([q[i][c] for c in range(num_colors)]), 1)
-    for i in range(num_region)
-]
-
+reg_constraints = one_hot(q[1:], axis=1)  # ダミー都道府県は定式化には含めない
+    
 # 隣接する領域間の制約
-adj_constraints = [
-    # 都道府県コードと配列インデックスは1ずれてるので注意
-    penalty(q[i][c] * q[j - 1][c])
-    for i in range(num_region)
-    for j in jm.adjacent(i + 1)  # j: 隣接している都道府県コード
-    if i + 1 < j
-    for c in range(num_colors)
-]
+adj_constraints = amplify_sum(
+    equal_to(q[i, :] * q[j, :], 0, axis=())
+    for i in range(1, num_region)
+    for j in jm.adjacent(
+        i
+    )  # j: 都道府県コード i の都道府県と隣接している都道府県コード
+    if i < j  # type: ignore
+)
+    
+model = reg_constraints + adj_constraints
 
-constraints = sum(reg_constraints) + sum(adj_constraints)
-
-model = BinaryQuadraticModel(constraints)
-result = solver.solve(model)
+# ソルバー実行
+from amplify import FixstarsClient, solve
+from datetime import timedelta
+    
+client = FixstarsClient()
+client.parameters.timeout = timedelta(milliseconds=5000)  # タイムアウト 5000 ms
+client.token = "AE/gd0y09RhHp0H0EcefLvtxYhbwl7Nk0US"  #20260113で有効
+    
+# 求解と結果の取得
+result = solve(model, client)
 if len(result) == 0:
-    raise RuntimeError("Any one of constraints is not satisfied.")
+    raise RuntimeError("Some constraints are unsatisfied.")
 
-values = result[0].values
-q_values = decode_solution(q, values, 1)
+q_values = q.evaluate(result.best.values)
 
-color_indices = np.where(np.array(q_values) == 1)[1]
-color_map = {jm.pref_names[i + 1]: colors[color_indices[i]] for i in range(len(color_indices))}
+import numpy as np
+
+color_indices = (q_values[1:] @ np.arange(num_colors)).astype(
+    int
+)  # q_values の最初の行はダミー都道府県のものなので捨てる
+color_map = {
+    jm.pref_names[region_idx]: colors[color_idx]
+    for region_idx, color_idx in enumerate(
+        color_indices, start=1
+    )  # region_idx は 1 スタートなことに注意
+}
+
+import matplotlib.pyplot as plt
 
 plt.rcParams["figure.figsize"] = 6, 6
 plt.imshow(jm.picture(color_map))
